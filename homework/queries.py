f4dcea2a6e0a94d86ed7c66ab913646f0a5f8eda
Moni Restre <monicarestrepoleon@gmail.com>
2024-11-13 09:10:16 -0500

    done

--- homework/queries.py
+++ homework/queries.py
@@ -1,14 +1,45 @@
-"""Taller evaluable"""
+from mapreduce import run_mapreduce_job  # type: ignore
 
-# pylint: disable=broad-exception-raised
-# pylint: disable=import-error
+#
+# Columns:
+# total_bill, tip, sex, smoker, day, time, size
+#
 
+
+
+
+def mapper_query_1(sequence):
+    """Mapper"""
+    result = []
+    for index, (_, row) in enumerate(sequence):
+        if index == 0:
+            result.append((index, row.strip() + ",tip_rate"))
+        else:
+            row_values = row.strip().split(",")
+            total_bill = float(row_values[0])
+            tip = float(row_values[1])
+            tip_rate = tip / total_bill
+            result.append((index, row.strip() + "," + str(tip_rate)))
+    return result
+
+
+def reducer_query_1(sequence):
+    """Reducer"""
+    return sequence
 
 #
 # ORQUESTADOR:
 #
 def run():
     """Orquestador"""
+
+    run_mapreduce_job(
+        mapper=mapper_query_1,
+        reducer=reducer_query_1,
+        input_directory="files/input",
+        output_directory="files/query_1",
+    )
+    print("Done!")
 
 
 if __name__ == "__main__":
